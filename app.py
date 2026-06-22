7cab7f5da2c9dde7b6e95ec6064b940526acf92e
Praveen raj <pr186360@gmail.com>
2026-06-23 00:56:24 +0530

    Initialize Flask web application in app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__web-applicaton__)
+
+@app.route('/')
+def home():
+    return "Hello World"
+  def create_app():
+    app = Flask(__web-application__)
+    return app
