30f2457be57640907f011f5b834d0109f91b3379
Praveen raj <pr186360@gmail.com>
2026-06-23 00:59:24 +0530

    Fix Flask app initialization and home route response
    Corrected the spelling of 'application' and updated the home route response.

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask import Flask
 
-app = Flask(__web-applicaton__)
+app = Flask(__web-application__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Hello World"
-  def create_app():
-    app = Flask(__web-application__)
-    return app
+    return "Hello"
